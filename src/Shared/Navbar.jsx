a0d3101ed8d012362efa83cc0d7d0754a1001a56
ArunRoy404 <arunroy0065@gmail.com>
2025-10-22 10:37:11 +0600

    refactor: navbar made sticky

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,27 +1,49 @@
+'use client'
+
 import NavbarActions from '@/components/Navbar/NavbarActions';
 import NavLinks from '@/components/Navbar/NavLinks';
 import LogoIcon from '@/components/svg/LogoIcon';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
+import { useEffect, useState } from 'react';
+
 
 const Navbar = () => {
+    const [isVisible, setIsVisible] = useState(true)
+    const [lastScrollY, setLastScrollY] = useState(0)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScroll = window.scrollY
+
+            if (currentScroll > lastScrollY && currentScroll > 80) {
+                setIsVisible(false)
+            } else {
+                setIsVisible(true)
+            }
+            setLastScrollY(currentScroll)
+        }
+
+
+        window.addEventListener("scroll", handleScroll)
+        return () => window.removeEventListener("scroll", handleScroll)
+    }, [lastScrollY])
+
+
+    console.log(isVisible);
     return (
-        <nav className='container mx-auto flex items-center justify-between py-6'>
-            <LogoIcon />
-
-            <ul
-                className='flex items-center gap-8 bg-primary py-3 px-6 rounded-md'
-            >
-                <NavLinks />
-            </ul>
-
-            <NavbarActions />
-        </nav>
+        <header
+            className={`bg-neutral/80 backdrop-blur-md sticky top-0 z-200 transition-transform duration-500 ease-in-out 
+            ${isVisible ? "translate-y-0" : "-translate-y-full"}`}
+        >
+            <nav className='container mx-auto flex items-center justify-between py-6'>
+                <LogoIcon />
+                <ul
+                    className='flex items-center gap-8 bg-primary py-3 px-6 rounded-md'
+                >
+                    <NavLinks />
+                </ul>
+                <NavbarActions />
+            </nav>
+        </header>
     );
 };
 
