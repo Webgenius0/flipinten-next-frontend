a92e2bab51f0185e4f13f830cb46b76acfe0ae7f
ArunRoy404 <arunroy0065@gmail.com>
2025-10-25 14:06:38 +0600

    feat: createo marketplace hero section created

---
+++ src/pages/Landing/MarketplacePage.jsx
@@ -0,0 +1,12 @@
+import CreatorHero from "@/components/MarketplaceComponents/CreatorHero";
+import React from "react";
+
+const MarketplacePage = () => {
+  return (
+    <>
+      <CreatorHero />
+    </>
+  );
+};
+
+export default MarketplacePage;
