372333894a3647212969200481a82212ff34a650
ArunRoy404 <arunroy0065@gmail.com>
2025-10-26 11:09:40 +0600

    feat: Advetisers stat section added

--- src/pages/Landing/AdvertisersPage.jsx
+++ src/pages/Landing/AdvertisersPage.jsx
@@ -1,10 +1,12 @@
 import AdvertisersHero from "@/components/AdvertisersPageComponent/AdvertisersHero";
+import AdvertisersStatsSection from "@/components/AdvertisersPageComponent/AdvertisersStatsSection";
 import React from "react";
 
 const AdvertisersPage = () => {
   return (
     <>
       <AdvertisersHero />
+      <AdvertisersStatsSection/>
     </>
   );
 };
