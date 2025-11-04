bb189aa35d94209cb1da20e90c7fbdb90584f1ea
Sajnin14 <sajninsaima@gmail.com>
2025-11-04 10:10:45 +0600

    yt services dashboard packages page done

--- src/app/yt-services-dashboard/packages/page.jsx
+++ src/app/yt-services-dashboard/packages/page.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import YtServicesDashPackages from "@/pages/YtServicesDashboardPages/YtServicesDashPackages";
+import React from "react";
 
 export default function page() {
-  return (
-    <div>packages</div>
-  )
+  return <YtServicesDashPackages />;
 }
