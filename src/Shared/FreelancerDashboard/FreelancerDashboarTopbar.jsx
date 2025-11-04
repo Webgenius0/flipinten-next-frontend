39c40b12a37f16a1b733aded8cca05a2e47fc317
Sajnin14 <sajninsaima@gmail.com>
2025-11-04 15:22:13 +0600

    freelancer dashboard layout created

---
+++ src/Shared/FreelancerDashboard/FreelancerDashboarTopbar.jsx
@@ -0,0 +1,44 @@
+"use client";
+import NotificationSVG from "@/components/svg/NotificationSVG";
+import Image from "next/image";
+import UserAvatar from "@/assets/images/user-avatar.png";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+export default function FreelancerDashboarTopbar() {
+  const currentPathName = usePathname();
+
+  return (
+    <div className="w-full h-full py-5 px-10 flex items-center justify-between">
+      <div>
+        <h2 className="text-black text-3xl font-semibold capitalize">
+          {currentPathName === "/freelancer-dashboard" ? (
+            <span>Welcome back, Manoj Manju</span>
+          ) : (
+            <span>{currentPathName?.split("/")[2]?.replace("-", " ")}</span>
+          )}
+        </h2>
+      </div>
+
+      <div className="flex items-center gap-6">
+        <Link
+          onClick={() => toast("Feature coming soon")}
+          href={"#"}
+          className="size-10 flex items-center justify-center border border-[#C1C4CC] rounded-lg bg-[#F5F6F7] cursor-pointer"
+        >
+          <NotificationSVG />
+        </Link>
+
+        <Link href={"#"} className="size-[50px] rounded-full">
+          <Image
+            src={UserAvatar}
+            width={"40px"}
+            height={"40px"}
+            alt="User Image"
+          />
+        </Link>
+      </div>
+    </div>
+  );
+}
