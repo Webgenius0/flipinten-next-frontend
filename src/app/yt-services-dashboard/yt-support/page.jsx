92a6e8220ea152475cc2df730e8ed48a8d781c49
Sajnin14 <sajninsaima@gmail.com>
2025-11-04 11:49:53 +0600

    yt dashboard support page done

--- src/app/yt-services-dashboard/yt-support/page.jsx
+++ src/app/yt-services-dashboard/yt-support/page.jsx
@@ -1,5 +1,6 @@
+import YtServicesDashSupportPage from "@/pages/YtServicesDashboardPages/YtServicesDashSupportPage";
 import React from "react";
 
 export default function page() {
-  return <div>yt-support</div>;
+  return <YtServicesDashSupportPage/>;
 }
